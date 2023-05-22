b479850b1851333f8bbea207c29235f9be318a7c
Bruno Benavent Colomo <70907703+brunobenavent@users.noreply.github.com>
2023-05-22 15:08:45 +0200

    fin de la app

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,37 @@
+//Referencias html
+
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket')
+const btnCrear = document.querySelector('button')
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    btnCrear.disabled = false
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnCrear.disabled = true
+});
+
+
+socket.on('ultimo-ticket', (ultimo)=>{
+    lblNuevoTicket.innerText = 'ticket ' + ultimo 
+})
+
+
+btnCrear.addEventListener( 'click', () => {
+
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText = ticket 
+    });
+    
+
+});
